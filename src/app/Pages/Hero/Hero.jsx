6cbf58cfc30f77da20f3f106a622bf40b549fad2
RickyAnchores <ricky23anchores@gmail.com>
2024-08-18 15:04:11 +0200

    Test Dashboard
    Test dashboard right routing

--- src/app/Pages/Hero/Hero.jsx
+++ src/app/Pages/Hero/Hero.jsx
@@ -7,6 +7,7 @@ const Hero = () => {
         <div className="user">
           <h1 className='Title text-4xl md:text-8xl'>NXT USER</h1>
           <Link href="./user">Go to user</Link>
+          <Link href="./dashboard">Go to dashboard</Link>
         </div>
         <img className='HeroImage w-[400px]' src="https://images.unsplash.com/photo-1568733873715-f9d497a47ea0?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w0Nzg3MDV8MHwxfHNlYXJjaHw2fHxibGFjayUyMGFuZCUyMHdoaXRlJTIwZGlnaXRhbHxlbnwwfHx8fDE3MTg2MjA1MzF8MA&ixlib=rb-4.0.3&q=80&w=1080" alt="HeroImage" />
     </div>
---
+++ src/app/dashboard/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='dashboard'>
+        <h1>This is DASHBOARD</h1>
+    </div>
+  )
+}
+
+export default page;
